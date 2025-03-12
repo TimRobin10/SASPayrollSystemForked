c285801103a279e2106c1743bba65882c1009510
AJ-Sabado <a.sabado.522151@umindanao.edu.ph>
2025-03-12 19:52:34 +0800

    Minor changes to IUnitOfWork

--- ServicesLayer/IUnitOfWork.cs
+++ ServicesLayer/IUnitOfWork.cs
@@ -1,8 +1,10 @@
 ﻿using DomainLayer.Models.Attendance;
+using DomainLayer.Models.ChangePasswordRequest;
 using DomainLayer.Models.Department;
 using DomainLayer.Models.Employee;
 using DomainLayer.Models.Leave;
 using DomainLayer.Models.NewUserRequest;
+using DomainLayer.Models.Payroll;
 using DomainLayer.Models.Role;
 using DomainLayer.Models.User;
 using ServicesLayer.Common;
@@ -15,16 +17,18 @@ namespace ServicesLayer
         IUserModel? CurrentUser { get; }
         IBaseServices<DepartmentModel> DepartmentRepository { get; }
         IBaseServices<EmployeeModel> EmployeeRepository { get; }
+        IBaseServices<ForgotPasswordRequestModel> ForgotPasswordRequestRepository { get; }
         IBaseServices<LeaveModel> LeaveRepository { get; }
         IBaseServices<NewUserRequestModel> NewUserRequestRepository { get; }
+        IBaseServices<PayrollModel> PayrollRepository { get; }
         IBaseServices<RoleModel> RoleRepository { get; }
         IBaseServices<UserModel> UserRepository { get; }
 
         Task ApproveNewUserRequest(string requestEmail, string roleName = null);
+        Task ForgotPasswordRequest(string username, string email, string password, string confirmPassword);
         Task InitialSeeding();
         Task LoginUser(string username, string password);
         Task NewUserRequest(string username, string password, string email);
-        Task ForgotPasswordRequest(string username, string email, string password, string confirmPassword);
         void Save();
     }
 }
